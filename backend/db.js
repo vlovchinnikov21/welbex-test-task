3fabf78623cb47be1bcde81c19f3230491ff7596
Vladimir Ovchinnikov <metalass21@yandex.ru>
2022-10-24 23:08:38 +0300

    fix: добавить переменную из env для подключения к БД

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,12 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: 'postgres://jrskmdjobstsft:0631d8e816e212e99ad01d5b6f0a52ad7bd6c159ac6e0bf63b659ddbfed25da5@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/ddhr4mc2han9jd',
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
+console.log(process.env.DATABASE_URL);
 
 
 module.exports = client
